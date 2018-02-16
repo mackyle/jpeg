27750aeb9baf26fe711194475cef2d57402233f4
Independent JPEG Group Software <jpeg-info@jpegclub.org>
2018-02-16 14:39:09 +0000

    droppatch additions for release 9c from jpegclub.org
      http://jpegclub.org/droppatch.v9c.tar.gz:
        size:  84808 bytes
         md5:  b78421ffa5624e9ae90374301d82f123
        sha1:  44b61e869d8d8dfa051b8516755a55e5617e27ec
      sha256:  5d351274fb29dd155b53572d15a70e9295724b0fa18956c6079e1c9aa1302479

--- transupp.c
+++ transupp.c
@@ -1,7 +1,7 @@
 /*
  * transupp.c
  *
- * Copyright (C) 1997-2017, Thomas G. Lane, Guido Vollbeding.
+ * Copyright (C) 1997-2018, Thomas G. Lane, Guido Vollbeding.
  * This file is part of the Independent JPEG Group's software.
  * For conditions of distribution and use, see the accompanying README file.
  *
